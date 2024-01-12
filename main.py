e82a7c8a66d89f059ce228eb6ef52cbfd74a7d6f
Newturno <pongkorn_set@hotmail.com>
2024-01-12 10:39:19 +0700

    Update authorization

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import urllib.request
 
 api_endpoint = "https://threed-model-management.onrender.com/model-management-system/internal/ml/classification/store"
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJCbnVIRzQxbXpkdE1ZMjhuM3JSeCIsIm9yZ2FuaXphdGlvbklkIjoieU9oOWhBYmQ0RmZ1OTRtUHBLcjgiLCJpYXQiOjE3MDEwNjE5NTl9.cZ2xkoMJzvyaMMUYYm15XiG4xA9YmFS8fuZJBpf6d4Y"
+    "Authorization": "Bearer"
 }
 # Make a GET request to the API
 response = requests.post(api_endpoint,headers=headers)
